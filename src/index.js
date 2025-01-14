9e6d2eaae477ec7ad68d46e340225fdbcb4779b2
Mrutyunjaya <satapathymrutyunjaya915@gmail.com>
2025-01-14 22:26:22 +0530

    Initial discussion on UTILS, custom error api and their responses and a lil-bit of middlewares

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log("Hii");
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import mongoose from "mongoose";
+
+dotenv.config(
+    {
+        path : "./env"
+    }
+)
+connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.error("Error:- ", error);
+        throw error;
+        
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port:- ${process.env.PORT}`);
+        
+    })
+    
+})
+.catch((error) =>{
+    console.error("MONGO-DB Connection error ", error)
+})
+
+
+/*
+import express from "express";
+const app = express()
+;( async () => {
+    try{
+       await  mongoose.connect(`process.env.MONGODB_URI/${DB_NAME}`);
+
+       app.on("Error ", (error) =>{
+            console.log("Error:- ", error);
+            throw error
+       })
+
+       app.listen(process.env.PORT, () =>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+        
+       })
+    }catch(error){
+        console.error("Error:- ", error)
+        throw error
+    }
+})()
+*/
